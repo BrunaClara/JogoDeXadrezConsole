9c25ebb4ff33a9c93ab7a62b9395f7a72742c024
Bruna Clara <bruna.clara@squadra.com>
2022-08-26 10:52:11 -0300

    Implementando restrições de movimento.

--- JogoDeXadrez/Tabuleiros/Peca.cs
+++ JogoDeXadrez/Tabuleiros/Peca.cs
@@ -21,11 +21,32 @@ namespace JogoDeXadrez.Tabuleiros
             QtdMovimentos = 0;
         }
 
-        public abstract bool[,] movimentosPossiveis();
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i=0; i<Tab.Linhas; i++)
+            {
+                for (int j=0; j<Tab.Colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.Linha, pos.Coluna];
+        }
 
         public void incrementarQtdMovimentos()
         {
             QtdMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
     }
 }
