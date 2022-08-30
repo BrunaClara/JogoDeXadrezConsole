3ce20e36b9aa0b223e6cbc7bc40a2ef2c90c076f
Bruna Clara <bruna.clara@squadra.com>
2022-08-30 14:19:53 -0300

    Acrescentando as demais peças.

---
+++ JogoDeXadrez/Xadrez/Peao.cs
@@ -0,0 +1,92 @@
+﻿using JogoDeXadrez.Tabuleiros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDeXadrez.Xadrez
+{
+    class Peao : Peca
+    {
+        private PartidaDeXadrez Partida;
+
+        public Peao(Tabuleiro tabu, Cor cor, PartidaDeXadrez partida) : base(cor, tabu)
+        {
+            Partida = partida;
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+
+        private bool existeInimigo(Posicao pos)
+        {
+            Peca p = Tab.peca(pos);
+            return p != null && p.Cor != Cor;
+        }
+
+        private bool livre(Posicao pos)
+        {
+            return Tab.peca(pos) == null;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            if (Cor == Cor.Branca)
+            {
+                pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+                if (Tab.posicaoValida(pos) && livre(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(Posicao.Linha - 2, Posicao.Coluna);
+                Posicao p2 = new Posicao(Posicao.Linha - 1, Posicao.Coluna);
+                if (Tab.posicaoValida(p2) && livre(p2) && Tab.posicaoValida(pos) && livre(pos) && QtdMovimentos == 0)
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+                if (Tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+                if (Tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+            }
+            else
+            {
+                pos.definirValores(Posicao.Linha + 1, Posicao.Coluna);
+                if (Tab.posicaoValida(pos) && livre(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(Posicao.Linha + 2, Posicao.Coluna);
+                Posicao p2 = new Posicao(Posicao.Linha + 1, Posicao.Coluna);
+                if (Tab.posicaoValida(p2) && livre(p2) && Tab.posicaoValida(pos) && livre(pos) && QtdMovimentos == 0)
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+                if (Tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.definirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+                if (Tab.posicaoValida(pos) && existeInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+            }
+            return mat;
+        }
+    }
+}
